e7e8245616863da75e2af5989d364ae240771fa2
Kristina Rudyte <rkristina32@yahoo.com>
2022-03-30 20:31:48 +0300

    4th task

---
+++ js/04-uzduotis.js
@@ -0,0 +1,11 @@
+/*4. Duoti du sveikieji skaičiai a ir b. Parašykite programą, kuri didesnį skaičių pamažintu
+vienetu, o mažesnį padidintu vienetu.*/
+
+let a = 22;
+let b = 45;
+
+if (a>b)
+{console.log(++a, --b);}
+else {console.log(--a, ++b);}
+
+
