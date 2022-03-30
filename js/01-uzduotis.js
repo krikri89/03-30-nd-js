39f0f0a0bde417ec0cf1f7a4a148585712a269ce
Kristina Rudyte <rkristina32@yahoo.com>
2022-03-30 15:35:01 +0300

    1st task

---
+++ js/01-uzduotis.js
@@ -0,0 +1,11 @@
+/*1. Žinomas skaičius n. Parašykite programą, kuri nustatytų ar skaičius yra blogas ar geras?
+Neigiamas – „Blogas“, teigiamas „Geras“. Pasitikrinkite: Kai n = 20, turėtume gauti:
+Skaičius geras.*/
+
+const n = -20;
+if (n < 0) {
+    console.log('blogas');
+} else {
+    console.log('geras');
+}
+
