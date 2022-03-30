eb332b2ba880b0c3d141b39ce12bd5cf8cf0708d
Kristina Rudyte <rkristina32@yahoo.com>
2022-03-30 17:29:04 +0300

    task 2

---
+++ js/02-uzduotis.js
@@ -0,0 +1,20 @@
+/*2. Mokinys eina gatve ir prieina sankryžą reguliuojamą šviesoforu. Sudarykite programą,
+kuri nustatytu, kaip turi pasielgti mokinys? Žinome šviesoforo spalvos kodą n, 1-žalia, 2-geltona, 3-raudona. 
+Pasitikrinkite: Kai n= 2, turėtume gauti: Palaukite.
+*/
+
+const spalva = '2';
+
+switch (spalva) {
+  case "1":
+    console.log('zalia go');
+    break;
+  case "2":
+    console.log(' geltona wait');
+    break;
+  case "3":
+    console.log('raudona stop');
+    break;
+default:
+    console.log('leave');
+}
